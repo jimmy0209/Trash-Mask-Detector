50a43b346b4c9fa0e5f0fc8c47d67bcf27852856
FENG Yurong <53307080+JazzyFeng@users.noreply.github.com>
2022-03-29 17:20:09 +0800

    first

---
+++ detector/test.sh
@@ -0,0 +1,2 @@
+python3 detector.py test --dataset=../data --model=taco --round 0 --class_map=./taco_config/map_10.csv
+
---
+++ detector/train.sh
@@ -0,0 +1,2 @@
+python3 detector.py train --dataset=../data --model=coco --class_map=./taco_config/map_10.csv --round 0
+
